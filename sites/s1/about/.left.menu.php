428f78bca86db45361a3a0f51151263bcd0a6515
Aladser <aladser@gmail.com>
2024-05-28 20:56:16 +0900

    обновлено главное меню

---
+++ sites/s1/about/.left.menu.php
@@ -0,0 +1,33 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$aMenuLinks = Array(
+	Array(
+		"Документы", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Отзывы", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Вопрос-ответ", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Связаться с менеджером", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+);
+?>
--- sites/s1/about/index.php
+++ sites/s1/about/index.php
@@ -11,26 +11,7 @@
 
 
 <div class='container'>
-	<div><?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"main-menu", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "main-menu",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "2",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "N",
-		"MENU_CACHE_USE_GROUPS" => "N"
-	),
-	false
-);?></div>
-	
+	<p class="fs-2 fw-bold">Что-то о компании</p>
 </div>
 
 
