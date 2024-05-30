22f2c7fe50eca4c74927a08ec6b123ffe29008d9
Aladser <aladser@gmail.com>
2024-05-30 10:12:15 +0900

    добавлен каталог на главную

--- sites/s1/index.php
+++ sites/s1/index.php
@@ -8,84 +8,150 @@
 	$APPLICATION->SetPageProperty("not_show_nav_chain_custom", 'Y');
 	$APPLICATION->SetPageProperty("not_show_text_formatting", 'Y');
 ?>
-
-<div class='container'>
-
-<?$APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"slider-list", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "N",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
+<div class="container">
+	 <?$APPLICATION->IncludeComponent(
+		"bitrix:news.list",
+		"slider-list",
+		Array(
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"ADD_SECTIONS_CHAIN" => "Y",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "N",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"CHECK_DATES" => "Y",
+			"COMPONENT_TEMPLATE" => "slider-list",
+			"DETAIL_URL" => "",
+			"DISPLAY_BOTTOM_PAGER" => "Y",
+			"DISPLAY_DATE" => "Y",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"FIELD_CODE" => array(0=>"",1=>"",),
+			"FILTER_NAME" => "",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"IBLOCK_ID" => "27",
+			"IBLOCK_TYPE" => "slider",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"MESSAGE_404" => "",
+			"NEWS_COUNT" => "5",
+			"PAGER_BASE_LINK_ENABLE" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "N",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => ".default",
+			"PAGER_TITLE" => "Новости",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"PROPERTY_CODE" => array(0=>"PHOTO",1=>"MOBILE_PHOTO",2=>"",),
+			"SET_BROWSER_TITLE" => "N",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "N",
+			"SHOW_404" => "N",
+			"SORT_BY1" => "ACTIVE_FROM",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER1" => "DESC",
+			"SORT_ORDER2" => "ASC",
+			"STRICT_SECTION_CHECK" => "N"
+		)
+	);?> 
+	<br>
+	<div class="index-catalog__head">
+		<div class="index-catalog__title">Каталог</div>
+		<a href="#" class="index-catalog__link"> <span class="tablet-hide">Полный каталог</span> <span class="mobile-hide tablet-show">Смотреть полный каталог</span> </a>
+	</div>
+	<?$APPLICATION->IncludeComponent(
+		"bitrix:news.list", 
+		"catalog-auto-list", 
+		array(
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"ADD_SECTIONS_CHAIN" => "Y",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "36000000",
+			"CACHE_TYPE" => "A",
+			"CHECK_DATES" => "Y",
+			"DETAIL_URL" => "",
+			"DISPLAY_BOTTOM_PAGER" => "Y",
+			"DISPLAY_DATE" => "Y",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"FIELD_CODE" => array(
+				0 => "PREVIEW_PICTURE",
+				1 => "DETAIL_PICTURE",
+				2 => "",
+			),
+			"FILTER_NAME" => "",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"IBLOCK_ID" => "1",
+			"IBLOCK_TYPE" => "catalog_auto",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"MESSAGE_404" => "",
+			"NEWS_COUNT" => "5",
+			"PAGER_BASE_LINK_ENABLE" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "N",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => ".default",
+			"PAGER_TITLE" => "Новости",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"PROPERTY_CODE" => array(
+				0 => "MARK",
+				1 => "MODEL",
+				2 => "YEAR",
+				3 => "TYPE_CAR_BODY",
+				4 => "COLOR",
+				5 => "MILEAGE",
+				6 => "TRANSMISSION",
+				7 => "ENGINE",
+				8 => "ENGINE_VOLUME",
+				9 => "ENGINE_POWER",
+				10 => "DRIVE_UNIT",
+				11 => "PRICE_CNY",
+				12 => "VENDOR_CODE",
+				13 => "EQUIPMENT",
+				14 => "",
+			),
+			"SET_BROWSER_TITLE" => "Y",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "Y",
+			"SET_META_KEYWORDS" => "Y",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "Y",
+			"SHOW_404" => "N",
+			"SORT_BY1" => "ACTIVE_FROM",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER1" => "DESC",
+			"SORT_ORDER2" => "ASC",
+			"STRICT_SECTION_CHECK" => "N",
+			"COMPONENT_TEMPLATE" => "catalog-auto-list"
 		),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "27",
-		"IBLOCK_TYPE" => "slider",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "5",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "PHOTO",
-			1 => "MOBILE_PHOTO",
-			2 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "slider-list"
-	),
-	false
-);?>
-<br>
-<div class="index-catalog__head">
-	<div class="index-catalog__title">Каталог</div>
-	<a href="#" class="index-catalog__link">
-		<span class="tablet-hide">Полный каталог</span>
-		<span class="mobile-hide tablet-show">Смотреть полный каталог</span>
-	</a>
-</div>
-
+		false
+	);?>
+	<br>
 </div>
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
