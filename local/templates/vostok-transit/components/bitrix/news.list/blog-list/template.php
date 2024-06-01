24c41a9a7353a566391f090ae86ad157ca634f27
Aladser <aladser@gmail.com>
2024-06-01 22:41:54 +0900

    обновление блога

--- local/templates/vostok-transit/components/bitrix/news.list/blog-list/template.php
+++ local/templates/vostok-transit/components/bitrix/news.list/blog-list/template.php
@@ -25,23 +25,14 @@ $this->setFrameMode(true);
         </div>
         <div class="index-blog-tabs swiper swiper-initialized swiper-horizontal swiper-free-mode swiper-backface-hidden">
             <div class="index-blog-tabs-wrapper swiper-wrapper">
-                    <a href="#" class="index-blog-tab swiper-slide swiper-slide-active" style="margin-right: 11px;">
-                        Статьи
-                    </a>
-                    <a href="#" class="index-blog-tab swiper-slide swiper-slide-next" style="margin-right: 11px;">
-                        Новости
-                    </a>
-                    <a href="#" class="index-blog-tab swiper-slide" style="margin-right: 11px;">
-                        Обзоры
-                    </a>
-                    <a href="#" class="index-blog-tab swiper-slide" style="margin-right: 11px;">
-                        Рынок
-                    </a>
+                    <a href="#" class="index-blog-tab swiper-slide swiper-slide-active" style="margin-right: 11px;">Статьи</a>
+                    <a href="#" class="index-blog-tab swiper-slide swiper-slide-next" style="margin-right: 11px;">Новости</a>
+                    <a href="#" class="index-blog-tab swiper-slide" style="margin-right: 11px;">Обзоры</a>
+                    <a href="#" class="index-blog-tab swiper-slide" style="margin-right: 11px;">Рынок</a>
             </div>
         </div>
         <div class="index-blog-list">
-			<!-- большие карточки -->
-			<? for($i=0; $i<2; $i++): ?>
+			<? for($i=0; $i<count($arResult["ITEMS"]); $i++): ?>
 				<div class="index-blog-card" id="bx000<?=$i+1?>">
 					<div class="index-blog-card-img">
 						<img src="<?=$arResult["ITEMS"][$i]["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arResult["ITEMS"][$i]['NAME']?>" width="558" height="398" class="index-blog-card__image" loading="lazy">
@@ -53,22 +44,7 @@ $this->setFrameMode(true);
 					<div class="index-blog-card__title"><?=$arResult["ITEMS"][$i]['NAME']?></div>
 					<div class="index-blog-card__subtitle"><?=$arResult["ITEMS"][$i]["PREVIEW_TEXT"]?></div>
 					<a href="#" class="link-as-card"></a>
-					<div class="index-blog-card-button"><a href="#" class="index-blog-card__link">Подробнее</a></div>
-				</div>
-			<? endfor ?>
-			<!-- малые карточки -->
-			<? for($i=2; $i<5; $i++): ?>
-				<div class="index-blog-card" id="bx000<?=$i+1?>">
-					<div class="index-blog-card-img">
-						<img src="<?=$arResult["ITEMS"][$i]["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$arResult["ITEMS"][$i]['NAME']?>" alt="<?=$arResult["ITEMS"][$i]['NAME']?>" width="558" height="398" class="index-blog-card__image" loading="lazy">
-					</div>
-					<div class="index-blog-card-aside">
-						<div class="index-blog-card__date"><?=$arResult["ITEMS"][$i]["PROPERTIES"]['DATE']['VALUE']?></div>
-						<div class="index-blog-card__sections">| Новости</div>
-					</div>
-					<div class="index-blog-card__title"><?=$arResult["ITEMS"][$i]['NAME']?></div>
-					<div class="index-blog-card__subtitle"><?=$arResult["ITEMS"][$i]["PREVIEW_TEXT"]?></div>
-					<a href="#" class="link-as-card"></a>
+					<?if($i<2):?><div class="index-blog-card-button"><a href="#" class="index-blog-card__link">Подробнее</a></div><?endif?>
 				</div>
 			<? endfor ?>
     </div>
